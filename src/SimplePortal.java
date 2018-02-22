898e246f0fb54796faa274ffbc927261a110a91f
Aniquir <pawel9658@wp.pl>
2018-02-22 16:53:14 +0100

    added initial description of one class

---
+++ src/SimplePortal.java
@@ -0,0 +1,32 @@
+
+public class SimplePortal {
+
+    int[] positionFields;
+    int hitCount = 0;
+
+    public String check(String field){
+/**
+ * pobierz
+ * skonwertuj
+ * powtorz
+ * porownaj
+ * jesli
+ * inkrementuj
+ * sprawdz
+ * jesli
+ * w przeciwnym razie
+ * koniec jesli
+ * w przeciwnym razie
+ * zwroc
+ * koniec jesli
+ * koniec powtorz
+ */
+        return "";
+    }
+
+    public void setPositionFields(int[] positionFields) {
+//        pobierz
+//        przypisz
+        this.positionFields = positionFields;
+    }
+}
---
+++ src/SimplePortalTester.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Paweł on 2018-02-22.
+ */
+public class SimplePortalTester {
+}
